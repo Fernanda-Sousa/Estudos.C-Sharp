b333d2b10d92e3a8451ce4be5159b6237e9f2db3
Fernanda Sousa <fernanda.pss@live.com>
2024-01-17 15:54:52 -0300

    Exceptions

--- csharp_exception-main/csharp_exception/Program.cs
+++ csharp_exception-main/csharp_exception/Program.cs
@@ -1,16 +1,24 @@
 ﻿using csharp_exception.Titular;
 using csharp_exception.Contas;
-
+using csharp_exception;
 
 try
 {
-    ContaCorrente conta1 = new ContaCorrente(0, "1234-X");
+    ContaCorrente conta1 = new ContaCorrente(1, "1234-X");
+    conta1.Sacar(50);
+    Console.WriteLine(conta1.GetSaldo());
+    conta1.Sacar(150);
+    Console.WriteLine(conta1.GetSaldo());
 }
 catch(ArgumentException ex)
 {
     Console.WriteLine("Parâmetro com erro: " + ex.ParamName);//pra usar o nome do parametro tenho q passar ele no throw (ContaCorrente)
     Console.WriteLine("Não é possível criar uma conta com o número de agência menor ou igual a zero!");
     Console.WriteLine(ex.Message);
-    
+}
+catch(SaldoInsuficienteException ex)
+{
+    Console.WriteLine("Operação negada! Saldo Insuficiente!");
+    Console.WriteLine(ex.Message);
 }
 
---
+++ csharp_exception-main/csharp_exception/SaldoInsuficienteException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_exception
+{
+    public class SaldoInsuficienteException : Exception
+    {
+        public SaldoInsuficienteException(String mensagem) : base(mensagem) 
+        {
+        }
+    }
+}

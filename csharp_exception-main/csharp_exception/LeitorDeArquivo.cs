600a3cf525675eba3f02dd65130a8d628d2bd6df
Fernanda Sousa <fernanda.pss@live.com>
2024-01-18 15:01:18 -0300

    Exception-Final

---
+++ csharp_exception-main/csharp_exception/LeitorDeArquivo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_exception
+{
+    public class LeitorDeArquivo //: IDisposable //Syntax Sugar 
+    {
+        public string Arquivo { get; }
+
+        public LeitorDeArquivo(string arquivo)
+        {
+            Arquivo = arquivo;
+
+            // throw new FileNotFoundException(); 
+
+            Console.WriteLine("Abrindo arquivo: " + arquivo);
+        }
+
+        public string LerProximaLinha()
+        {
+            Console.WriteLine("Lendo linha. . .");
+            throw new IOException(); 
+
+            return "Linha do arquivo";
+        }
+
+        public void Fechar() //Dispose()
+        {
+            Console.WriteLine("Fechando arquivo.");
+        }
+    }
+}
